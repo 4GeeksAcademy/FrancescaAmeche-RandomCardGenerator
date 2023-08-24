8bcae33dd357dbd39773da9056a9ceefbc7cc756
FrancescaAmeche <121627825+FrancescaAmeche@users.noreply.github.com>
2023-08-24 02:08:42 +0000

    random card number and suit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  function randomCardGenerator() {
+    let cardSuits = ["♦", "♥", "♠", "♣"];
+    let randomSuits = cardSuits[Math.floor(Math.random() * cardSuits.length)];
+    document.getElementById("suitTop").textContent = randomSuits;
+    document.getElementById("suitBottom").textContent = randomSuits;
+
+    let cardNumber = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    document.getElementById("number").textContent =
+      cardNumber[Math.floor(Math.random() * cardNumber.length)];
+  }
+  randomCardGenerator();
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
